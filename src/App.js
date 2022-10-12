812f404fc781b292e6fcd99c07a09f4b22f943cb
emanuel ibañes <ibanes.emanuel@gmail.com>
2022-10-11 22:59:27 -0300

    modificacion de archivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './routes/Home/Home';
+import Navigation from './routes/Navigation/Navigation';
+import LoginRef from './routes/Login/LoginRef';
+import UbicationCreation from './routes/Ubication/UbicationCreation';
+import UbicationDisplay from './routes/Ubication/UbicationDisplay';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path='login' element={<LoginRef />} />
+          <Route path='ubication/:id' element={<UbicationDisplay />} />
+          <Route path='ubication/create' element={<UbicationCreation />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
+import { UbicationProvider } from "./context/UbicationContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <UbicationProvider>
+          <App />
+        </UbicationProvider>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
